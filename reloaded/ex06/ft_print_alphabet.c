81e4dbe4d22c8cbe5594bb403dddc29d7f02026c
Antonio Javier Andreu Peralta <antandre@car10s3.42barcelona.com>
2024-06-06 14:00:06 +0200

    ex00 - 12

---
+++ reloaded/ex06/ft_print_alphabet.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antandre <antandre@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/06 13:53:50 by antandre          #+#    #+#             */
+/*   Updated: 2024/06/06 13:54:09 by antandre         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+	void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_alphabet(void)
+{
+
+	char	i;
+
+	i = 'a';
+	while (i <= 'z')
+	{
+		ft_putchar(i);
+		i++;
+	}
+}
---
+++ reloaded/ex18/ft_print_params.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antandre <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/06 13:37:17 by antandre          #+#    #+#             */
+/*   Updated: 2024/06/06 13:43:38 by antandre         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(&str[i]);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+
+	if (argc > 1)
+	{
+		i = 0;
+		while (argv
+	}
+}
