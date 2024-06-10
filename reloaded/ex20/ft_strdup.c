26ae52db5cf7b4ef7030e90130a9132759a8138e
Javier Andreu <andreujavier99@gmail.com>
2024-06-10 15:49:20 +0200

    strdup + range

---
+++ reloaded/ex20/ft_strdup.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antandre <antandre@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/10 14:49:45 by antandre          #+#    #+#             */
+/*   Updated: 2024/06/10 15:09:39 by antandre         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_strlen(char *s1)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	char *dup;
+	int	i;
+
+	dup = (char *)malloc((ft_strlen(src) + 1) * sizeof(char));
+	if (dup == NULL)
+		return (NULL);
+	i = 0;
+	while (src[i])
+	{
+		dup[i] = src[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	char *s1 = "Hello, world";
+
+	printf("%s", ft_strdup(s1));
+}
