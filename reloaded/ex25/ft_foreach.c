53a93ae25387a53ba7c1e5cf043bc72fe21b2392
Javier Andreu <andreujavier99@gmail.com>
2024-06-12 12:30:26 +0200

    ft_foreach

--- reloaded/ex25/ft_foreach.c
+++ reloaded/ex25/ft_foreach.c
@@ -6,30 +6,18 @@
 /*   By: antandre <antandre@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 15:20:09 by antandre          #+#    #+#             */
-/*   Updated: 2024/06/11 15:42:50 by antandre         ###   ########.fr       */
+/*   Updated: 2024/06/12 12:29:50 by antandre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_foreach(int *tab, int length, void (*f)(int))
 {
-	unsigned int	i;
+	int	i;
 
-	i = 1;
-	while (i <= length)
+	i = 0;
+	while (i < length)
 	{
-		(*f)(tab[i]);
+		(f)(tab[i]);
 		i++;
 	}
 }	
-
-#include <stdlib.h>
-void	ft_putnbr(int nb);
-
-int	main(void)
-{
-	int	*tab;
-
-	tab = (int *)malloc(50*sizeof(int));
-	ft_foreach(tab, 50, &ft_putnbr);
-	free(tab);
-}
