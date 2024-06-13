0c1836fba066d745c6cad5fa3585f8a2720e946b
Javier Andreu <andreujavier99@gmail.com>
2024-06-13 17:18:45 +0200

    todo mal

--- reloaded/ex27/ft_display_file.c
+++ reloaded/ex27/ft_display_file.c
@@ -6,46 +6,55 @@
 /*   By: antandre <antandre@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 15:16:49 by antandre          #+#    #+#             */
-/*   Updated: 2024/06/13 13:31:41 by antandre         ###   ########.fr       */
+/*   Updated: 2024/06/13 16:56:22 by antandre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <stddef.h>
+#include <unistd.h>
+
+
+void	ft_putstr_fd(int fd, char*str)
+{
+	int	i;
+
+	i = 0;
+	while(str[i])
+	{
+		write(fd, str + i, 1);
+		i++;
+	}
+}
 
 int	main(int argc, char **argv)
 {
-	FILE	*file;
-	unsigned char	*buffer[256];
+	int fd;
+	unsigned char	buffer[256];
 	size_t	result;
-	size_t	i;
 	
 	if (argc == 1)
 	{
-	fprintf(stderr, "File name missing.\n");
+	ft_putstr_fd(2, "File name missing.\n");
 	return (1);
 	}
 	if (argc > 2)
 	{
-	fprintf(stderr, "Too many arguments.\n");
+	ft_putstr_fd(2, "Too many arguments.\n");
 	return (1);
 	}
 	if (argc == 2)
 	{
-		file = fopen(argv[1], "rb");
-		result = fread(buffer, sizeof(buffer), 1, file);
-		i = 0;
-		if (result != 1)
+		fd = open(argv[1], 0_RDONLY);
+		while ((result = read(fd, 256, buffer)) > 0)
 		{
-			fprintf(stderr, "Cannot read file.\n");
-			return (1);
+			if (result < 1)
+			{	
+				write(2, "Cannot read file.\n", );
+				return (1);
+			}
+			ft_putstr_fd(fd, buffer);
 		}
-		while (fread(buffer, sizeof(buffer), 1, file))
-		{
-			printf("%s", buffer[i]);
-			i++;
-		}
-		fclose(file);
+		close(fd);
 		return (0);
 	}
 }
